856c9e167203c76de86e29723d68f5990e22ec5c
黎开松 <likaisong@meituan.com>
2018-11-07 08:59:02 +0800

    创建数据库及配置相关表

---
+++ app/src/main/java/com/baigaoli/easyweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.baigaoli.easyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by likaisong on 2018/11/7.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
